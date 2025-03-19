6761d368578713a3bad559238d227ccaa326ce79
Rubel Khan <rubellmy37@gmail.com>
2025-03-19 22:04:19 +0600

    Initial Commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,17 +6,20 @@ import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "$";
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem("token")? localStorage.getItem("token") : "");
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("token", token);
-  },[token])
+  }, [token]);
 
   return (
     <div className="bg-gray-100 min-h-screen">
